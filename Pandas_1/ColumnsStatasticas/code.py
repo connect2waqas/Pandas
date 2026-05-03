471ff5acc6eee4d828ece75e781e8042db243f94
Connect2waqas <waqasnadan972@gmail.com>
2026-05-03 13:19:38 +0500

    Add multiple Python scripts for data manipulation, cleaning, and analysis using Pandas adding Attributes for inspection

---
+++ Pandas_1/ColumnsStatasticas/code.py
@@ -0,0 +1,8 @@
+# Here we have to find the descriptive statistics of a dataframe:
+
+import pandas as pd
+
+data = pd.read_csv("sales_data_sample.csv" , encoding="latin1")
+
+print(data.describe())
+data[data["QUANTITYORDERED"].max() and data[data["PRICEEACH"].max()]]
